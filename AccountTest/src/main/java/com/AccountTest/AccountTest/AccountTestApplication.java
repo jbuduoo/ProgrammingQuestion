e34536b29d6b1bd76efb9669142125e7fbede22f
Jacky Dai <jbuduoo@gmail.com>
2024-04-13 11:19:52 +0800

    Add files via upload

---
+++ AccountTest/src/main/java/com/AccountTest/AccountTest/AccountTestApplication.java
@@ -0,0 +1,13 @@
+package com.AccountTest.AccountTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountTestApplication.class, args);
+	}
+
+}
---
+++ AccountTest/src/test/java/com/AccountTest/AccountTest/AccountTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AccountTest.AccountTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
